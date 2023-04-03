370d4fad241f5f4ee6afdb38db093e30800c5ac7
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-04-03 13:17:54 -0400

    3rd commit, the css is getting there...

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const searchCharacters = document.getElementById("searchCharacters")
 const characterList = document.getElementById("character-list")
 const characterInfo = document.querySelector("div")
 const daBody = document.querySelector("body")
-const belowUl = document.querySelector("ul")
 
 
 searchCharacters.addEventListener("click", (event) => {
-
-    event.preventDefault();
-
+    
+    
+    
     let characterName = characterInput.value
     characterInput.value = ""
     
@@ -20,23 +19,29 @@ searchCharacters.addEventListener("click", (event) => {
         return response.json()
     }).then(json => {
         findHPcharacter(json, characterName)
-    })
+    }).catch(error => console.log('Error! This a NoNo'));
 }) //Afterwards, the promise is fullfilled, the findHPcharacter will begin 
 
 
 const findHPcharacter = (json, characterName) => {
     characterList.innerHTML = ""
-
+    let put_the_ul = document.createElement("ul")
+    characterList.append(put_the_ul)
+    
+    if(typeof characterName !== 'string'){
+        window.alert(`Use letters only`)
+    }
+    
     let removePic = document.getElementById("daPictah")
     lowerCaseCharacterName = characterName.toLowerCase() //to make sure the search is not case-sensitive, I'm using toLowerCase() function
-
+    
     if (removePic) {
-    removePic.remove()
+        removePic.remove()
     }
-
+    
     
     for (nom of json){
-
+        
         if(nom.name.toLowerCase().includes(lowerCaseCharacterName)){
             let daName = nom.name
             let daSpecies = nom.species
@@ -44,7 +49,7 @@ const findHPcharacter = (json, characterName) => {
             let daActor = nom.actor
             let isAwizard = nom.wizard
             let wizardPic = nom.image
-
+            
             console.log(daName)
             console.log(daSpecies)
             console.log(daHouse)
@@ -58,7 +63,7 @@ const findHPcharacter = (json, characterName) => {
             let resultActor = document.createElement("li")
             let resultWizard = document.createElement("li")
             let imgElement = document.createElement("img")
-
+            
             
             imgElement.id = "daPictah"
             
@@ -67,6 +72,7 @@ const findHPcharacter = (json, characterName) => {
             imgElement.setAttribute(`alt`, daName)
             daBody.append(imgElement)
             
+            const belowUl = document.querySelector("ul") //I will use this for the Houses
             
             resultName.textContent = daName
             resultSpecies.innerHTML = daSpecies
@@ -89,8 +95,19 @@ const findHPcharacter = (json, characterName) => {
 
 
             if(daHouse === "Gryffindor"){
-                daBody.classList.add('gryffindor')
-
+                belowUl.setAttribute('id','gryffindor')
+            }
+                        
+            if(daHouse === "Ravenclaw"){
+                belowUl.setAttribute('id','ravenclaw')
+            }
+                        
+            if(daHouse === "Slytherin"){
+                belowUl.setAttribute('id','slytherin')
+            }
+                        
+            if(daHouse === "Hufflepuff"){
+                belowUl.setAttribute('id','hufflepuff')
             }
                         
         }
--- script2.js
+++ script2.js
@@ -3,14 +3,15 @@ const description = document.getElementById("description")
 const belowUl = document.querySelector("ul")
 
 
-fetch("https://hp-api.onrender.com/api/spells")
-  .then(response => response.json())
-  .then(data => {
-    for (let spell of data) {
-      let option = document.createElement("option")
-      option.textContent = spell.name;
-      option.value = spell.description;
-      dropdown.appendChild(option);
+let hexPromise = fetch("https://hp-api.onrender.com/api/spells")
+
+    hexPromise.then(response => response.json())
+        .then(data => {
+            for (let spell of data) {
+            let option = document.createElement("option")
+            option.textContent = spell.name;
+            option.value = spell.description;
+        dropdown.appendChild(option);
     }
   })
 
@@ -18,147 +19,8 @@ fetch("https://hp-api.onrender.com/api/spells")
 dropdown.addEventListener("change", function() {
   let selectedOption = dropdown.options[dropdown.selectedIndex]
   description.textContent = selectedOption.value
+
+  if(selectedOption.value.toLowerCase().includes("unforgivable")){
+    window.alert("This is an Unforgivable Curse! Proceed with caution!")
+  }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let demSpells = fetch(`https://hp-api.onrender.com/api/spells`)
-
-// demSpells.then(response => {
-//     return response.json()
-// }).then(json => {
-//     listOfspells(json)
-// })
-
-// let listOfspells = (json) => {
-//     for (let i =0; i <json.length; i++){
-//         let spell_options = document.createElement("option")
-//         spell_options.value = json[i].name
-//         spell_options.textContent = json[i].name;
-//         theDropdown.add(spell_options)
-//     }
-// }
-
-
-
-// dropdown.addEventListener("change", function() {
-//     let selectedOption = dropdown.options[dropdown.selectedIndex];
-//     description.textContent = selectedOption.value;
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const chosenSpell = document.getElementById("spells-to-pick")
-
-// chosenSpell.addEventListener("change",(event, json) =>{
-
-//         // console.log(event.target.value)
-
-//         for (let key in json){
-//             if(event.target.value === json){
-        
-//                 let daSpell = spel.name
-//                 let describeSpell = spel.description
-        
-//                 console.log(daSpell)
-//                 console.log(describeSpell)}
-
-                // let resultSpellName = document.createElement("li")
-                // let resultSpellInfo = document.createElement("li")
-                
-                
-                // resultSpellName.innerHTML = `<strong>Spell</strong>: ${spel.name}`
-                // //             resultSpellInfo.innerHTML = `<strong>What it does</strong>: ${describeSpell}`
-                
-                // belowUl.append(resultSpellName)
-                // //             belowUl.append(resultSpellInfo)
-            // }})
-    
-        // const selectedSpell = (json, targetedSpell) => {
-        //         // spellList.innerHTML = ""
-                
-        //         let selectedOption = theDropdown.options[theDropdown.selectedIndex].value;
-        
-        
-        
-                    
-                
-    // })
-
-
-
-
-
-
-// })
-
-
-
-
-
-
-
-
-//     [
-//         {
-//             "id": "c76a2922-ba4c-4278-baab-44defb631236",
-//             "name": "Aberto",
-//             "description": "Opens locked doors"
-//         },
-//         {
-//             "id": "06485500-d023-4799-93fd-77f2c3341aa3",
-//             "name": "Accio",
-//             "description": "Summons objects"
-//         },
-//         {
-//             "id": "acbc0ae1-12e1-4813-b51e-09d22de40475",
-//             "name": "Aguamenti",
-//             "description": "Summons water"
-//         },
-//         {
-//             "id": "c9d2f389-a419-4f7e-8d3d-254959638019",
-//             "name": "Alohomora",
-//             "description": "Unlocks objects"
-//         },
-//         {
-//             "id": "018429a5-15d5-41af-bf8f-98a966733d77",
-//             "name": "Anapneo",
-//             "description": "Clears someone's airway"
-//         }
-//   
